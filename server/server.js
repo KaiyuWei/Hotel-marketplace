51ebc3987a31b1c88d39541d814edb11c527da92
Kaiyu Wei <kaiyuwei@Zeenos-MacBook-Air.local>
2022-07-23 00:06:23 +0200

    work over

--- server/server.js
+++ server/server.js
@@ -4,12 +4,21 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 const morgan = require("morgan");
 require ('dotenv').config();
+/**
+ * possible issues:
+ * 1. no data/undefined - make sure to use body-parser or use express.json().
+ *      app.use(express.json());
+ * 2. CORS error - make sure you have cors applied in server.js:
+ *      app.use(cors())
+ */
 
 const app = express();
 
 //middlewares
 app.use(cors());
 app.use(morgan("dev"));
+app.use(express.json());  // apply this as middleware otherwise the server is not able to get json data sent from the frontend
+
 //route middleware
 fs.readdirSync('./routes').map((r) => 
     app.use('/api', require(`./routes/${r}`)));  // the reason of using Sync is that we want to make sure that all files are read
